9bb176decdb38b6d94a9f209448d66d9fc774d90
PowerAsh <ash.rawstron@gmail.com>
2018-09-23 13:09:33 -0600

    Add base UIBehavior.

---
+++ Code/UIBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace DraconianMarshmallows.UI
+{
+    public class UIBehavior : UIBehaviour
+    {
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        protected virtual void SetActive(bool active)
+        {
+            gameObject.SetActive(active);
+        }
+    }
+}
