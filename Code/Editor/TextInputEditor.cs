71fd42837030009a5582a98d007aebdf21436e9f
PowerAsh <ash.rawstron@gmail.com>
2018-09-23 19:32:37 -0600

    Add `TextInput` and it's inspector.

---
+++ Code/Editor/TextInputEditor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.UI;
+using UnityEditor;
+
+namespace DraconianMarshmallows.UI
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(TextInput), true)]
+    public class TextInputEditor : InputFieldEditor
+    {
+        SerializedProperty mErrorText;
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            mErrorText = serializedObject.FindProperty("errorTextComponent");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            base.OnInspectorGUI();
+            EditorGUILayout.PropertyField(mErrorText);
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
---
+++ Code/TextInput.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DraconianMarshmallows.UI
+{
+    public class TextInput : InputField
+    {
+        [SerializeField] private Text errorTextComponent;
+    }
+}
