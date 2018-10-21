71680600b4ed4ad613fe0e26e364bd5eca7d74c1
PowerAsh <ash.rawstron@gmail.com>
2018-10-21 16:34:39 -0600

    Add model with method for displaying info.

---
+++ Code/Modal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DraconianMarshmallows.UI
+{
+    public class Modal : UIBehavior
+    {
+        [SerializeField] private Text messageText; 
+        [SerializeField] private Button dismissButton;
+        [SerializeField] private Text dismissButtonText; 
+        
+        private Action onDismiss;
+
+        public void ShowInfoMessage(string message)
+        {
+            ShowInfoMessage(message, null);
+        }
+
+        public void SetDismissButton(string label)
+        {
+            dismissButtonText.text = label;
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        private void ShowInfoMessage(string message, Action onDismiss)
+        {
+            messageText.text = message;
+            if (onDismiss != null) this.onDismiss += onDismiss;
+            dismissButton.onClick.AddListener(internalOnDismiss);
+            SetActive(true);
+        }
+
+        private void internalOnDismiss()
+        {
+            if (onDismiss != null) onDismiss();
+            dismissButton.onClick.RemoveAllListeners(); 
+            SetActive(false);
+        }
+    }
+}
